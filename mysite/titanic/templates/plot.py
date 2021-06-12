53ee93d72efe3744db2da3f6f0ea3d634f10c614
AnSangYeong <asy19800510@gmail.com>
2021-06-12 15:02:05 +0900

    update code

--- mysite/titanic/templates/plot.py
+++ mysite/titanic/templates/plot.py
@@ -3,8 +3,15 @@ from mysite.titanic.models.service import Service
 
 class Plot(object):
 
-    dataset = Dataset
+    dataset = Dataset()
     service = Service()
 
     def __init__(self, fname):
         self.entity = self.service.new_model(fname)
+
+    def print_survived_dead(self):
+        this = self.entity
+        print(f'The data type of Train is {type(this)}.')
+        print(f'Columns of Train is {this.columns}.')
+        print(f'The top 5 superior data are {this.head}.')
+        print(f'The top 5 inferior data are {this.tail}.')
